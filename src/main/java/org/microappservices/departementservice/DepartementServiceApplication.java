4ae2e49bcebd435f9f1140890ea2e14b7821444b
maher <mahtrabelsi@gmail.com>
2023-11-26 20:43:45 +0100

    création du premier service *** departement-service ***

---
+++ src/main/java/org/microappservices/departementservice/DepartementServiceApplication.java
@@ -0,0 +1,16 @@
+package org.microappservices.departementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DepartementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartementServiceApplication.class, args);
+	}
+
+}
